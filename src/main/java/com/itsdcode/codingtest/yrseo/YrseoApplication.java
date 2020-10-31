421486f59cdcda95bd078f38dad05e4dc0d7272d
seoyoungrag <truezure@naver.com>
2020-10-31 18:34:48 +0900

    project init

---
+++ src/main/java/com/itsdcode/codingtest/yrseo/YrseoApplication.java
@@ -0,0 +1,13 @@
+package com.itsdcode.codingtest.yrseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YrseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YrseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsdcode/codingtest/yrseo/YrseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsdcode.codingtest.yrseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YrseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
